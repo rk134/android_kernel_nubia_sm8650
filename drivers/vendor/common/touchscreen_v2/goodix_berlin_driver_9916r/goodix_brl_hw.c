ccddf5e374bb8cf7f44dbf1e1157a345c380daca
Simranjeet Thind <quic_sthind@quicinc.com>
2023-03-22 16:09:59 -0700

    touch: pineapple: Replacing disable_irq with disable_irq_nosync
    disable_irq ensures existing instances of the IRQ handler have
    completed before returning which may create a deadlock if the handler
    doesn't find the resource.
    
    Change-Id: I7010d2a415f97de5ece8119b855c169ffba665b7
    Signed-off-by: Simranjeet Thind <quic_sthind@quicinc.com>

--- drivers/vendor/common/touchscreen_v2/goodix_berlin_driver_9916r/goodix_brl_hw.c
+++ drivers/vendor/common/touchscreen_v2/goodix_berlin_driver_9916r/goodix_brl_hw.c
@@ -314,7 +314,7 @@ static int brl_irq_enbale(struct goodix_ts_core *cd, bool enable)
 	}
 
 	if (!enable && atomic_cmpxchg(&cd->irq_enabled, 1, 0)) {
-		disable_irq(cd->irq);
+		disable_irq_nosync(cd->irq);
 		ts_info("Irq disabled");
 		return 0;
 	}
