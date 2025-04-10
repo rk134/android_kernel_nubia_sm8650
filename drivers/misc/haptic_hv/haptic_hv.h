1c1a726faa99245f136206efe0a42f7ade425f68
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-10 23:47:58 +0300

    misc: haptic_hv: hide info logs
    They're quite spammy.
    
    Change-Id: Idc63ddc0742a0ca44c82af7bf1a23ba7da83bf56

--- drivers/misc/haptic_hv/haptic_hv.h
+++ drivers/misc/haptic_hv/haptic_hv.h
@@ -230,7 +230,7 @@ struct aw_componet_codec_ops {
 	pr_err("[%s][%04d]%s: " format "\n", AW_I2C_NAME, __LINE__, __func__, ##__VA_ARGS__)
 
 #define aw_info(format, ...) \
-	pr_info("[%s][%04d]%s: " format "\n", AW_I2C_NAME, __LINE__, __func__, ##__VA_ARGS__)
+	pr_debug("[%s][%04d]%s: " format "\n", AW_I2C_NAME, __LINE__, __func__, ##__VA_ARGS__)
 
 #define aw_dbg(format, ...) \
 	pr_debug("[%s][%04d]%s: " format "\n", AW_I2C_NAME, __LINE__, __func__, ##__VA_ARGS__)
